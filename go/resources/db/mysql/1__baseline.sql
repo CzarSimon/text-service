c8670ce438550eae68083e29b27db7abe2d00564
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-07-27 01:01:47 +0200

    go - added postgres and mysql schema

--- go/resources/db/mysql/1__baseline.sql
+++ go/resources/db/mysql/1__baseline.sql
@@ -1,34 +1,34 @@
 -- +migrate Up
 CREATE TABLE `language` (
   `id`         VARCHAR(5) NOT NULL,
-  `created_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;;
 
 CREATE TABLE `translated_text` (
-  `id`         INTEGER PRIMARY KEY,
+  `id`         INT AUTO_INCREMENT PRIMARY KEY,
   `key`        VARCHAR(100) NOT NULL,
   `language`   VARCHAR(50) NOT NULL,
   `value`      TEXT NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `updated_at` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`language`) REFERENCES `language`(`id`),
   UNIQUE(`key`, `language`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;;
 
 CREATE TABLE `group` (
   `id`           VARCHAR(100) PRIMARY KEY,
-  `created_at`   DATETIME NOT NULL
-);
+  `created_at`   TIMESTAMP NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;;
 
 CREATE TABLE `text_group` (
-  `id`         INTEGER PRIMARY KEY,
+  `id`         INT AUTO_INCREMENT PRIMARY KEY,
   `text_key`   VARCHAR(100) NOT NULL,
   `group_id`   VARCHAR(100) NOT NULL,
-  `created_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
   FOREIGN KEY (`group_id`) REFERENCES `group`(`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;;
 
 -- +migrate Down
 DROP TABLE IF EXISTS `text_group`;
--- go/resources/db/postgres/1__baseline.sql
+++ go/resources/db/postgres/1__baseline.sql
@@ -1,17 +1,17 @@
 -- +migrate Up
 CREATE TABLE `language` (
   `id`         VARCHAR(5) NOT NULL,
-  `created_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `translated_text` (
-  `id`         INTEGER PRIMARY KEY,
+  `id`         SERIAL PRIMARY KEY,
   `key`        VARCHAR(100) NOT NULL,
   `language`   VARCHAR(50) NOT NULL,
   `value`      TEXT NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `updated_at` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`language`) REFERENCES `language`(`id`),
   UNIQUE(`key`, `language`)
@@ -19,14 +19,14 @@ CREATE TABLE `translated_text` (
 
 CREATE TABLE `group` (
   `id`           VARCHAR(100) PRIMARY KEY,
-  `created_at`   DATETIME NOT NULL
+  `created_at`   TIMESTAMP NOT NULL
 );
 
 CREATE TABLE `text_group` (
-  `id`         INTEGER PRIMARY KEY,
+  `id`         SERIAL PRIMARY KEY,
   `text_key`   VARCHAR(100) NOT NULL,
   `group_id`   VARCHAR(100) NOT NULL,
-  `created_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
   FOREIGN KEY (`group_id`) REFERENCES `group`(`id`)
 );
 
